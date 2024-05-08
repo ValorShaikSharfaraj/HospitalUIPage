cd65026d36eaecaecd413d4c26f999dbe0441a66
Shaik SHarfaraj Ahammed <codewithsharfaraj13@gmail.com>
2024-05-08 18:10:34 +0530

    Created Routes & defined

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import FindADoctor from "./pages/FindADoctor"
+import Apps from "./pages/Apps"
+import Testimonials from "./pages/Testimonials"
+import SignIn from "./pages/SignIn"
+
+import SignUp from "./pages/SignUp"
 
 export default function App() {
-  return (
-    <h1 className='text-red-600 font-bold '>App</h1>
-  )
+  return <BrowserRouter>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/findadoctor" element={<FindADoctor/>} />
+    <Route path="/apps" element={<Apps/>}/>
+    <Route path="/testimonials" element={<Testimonials/>}/>
+    <Route path="/sign-in" element={<SignIn/>}/>
+    <Route path="/sign-up" element={<SignUp/>}/>
+
+
+  </BrowserRouter>
 }
